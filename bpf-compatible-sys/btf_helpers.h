56bd6a27e86f91151faa961202b3e1ee5ace4ea3
officeyutong <yt.xyxx@gmail.com>
2023-05-28 19:00:43 +0000

    Define symbols to allow not linking the binary

--- bpf-compatible-sys/btf_helpers.h
+++ bpf-compatible-sys/btf_helpers.h
@@ -3,7 +3,9 @@
 
 #include <bpf/libbpf.h>
 
-int ensure_core_btf_with_tar_binary(const char** path, char* tar_bin, int tar_len);
+int ensure_core_btf_with_tar_binary(const char** path,
+                                    char* tar_bin,
+                                    int tar_len);
 
 int ensure_core_btf_with_linked_tar(const char** path);
 
@@ -16,5 +18,9 @@ static int ensure_core_btf(struct bpf_object_open_opts* opts) {
 static void cleanup_core_btf(struct bpf_object_open_opts* opts) {
     clean_core_btf_rs(opts->btf_custom_path);
 }
+
+// Allow for not linking the tar binary
+char _binary_min_core_btfs_tar_gz_start __attribute__((weak));
+char _binary_min_core_btfs_tar_gz_end __attribute__((weak));
 
 #endif  // _BTF_HELPER_H
